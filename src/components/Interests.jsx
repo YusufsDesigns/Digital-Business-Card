bc6246c226f5e329ce673497220accd915466f55
Yusuf Lawal <yusuf.olaoluwalawal@outlook.com>
2022-10-14 23:39:29 +0100

    New Commit

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -4,7 +4,7 @@ export default function Interests(){
     return(
         <div className="interests">
             <h3>Interests</h3>
-            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <p>Food expert. Reader. Internet fanatic. Coffee fanatic. Gamer, Anime otaku</p>
         </div>
     )
 }
