09f07f39bdcbe6f0cc3cf17fbd9227f6ff8b8caa
Yusuf Lawal <yusuf.olaoluwalawal@outlook.com>
2023-01-05 22:04:37 +0100

    New Commit

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -4,7 +4,7 @@ export default function Interests(props){
     return(
         <div className={props.lightMode ? 'light' : ''} id='interests'>
             <h3>Interests</h3>
-            <p>Food expert. Reader. Internet fanatic. Coffee fanatic. Gamer, Anime otaku</p>
+            <p>Food expert. Reader. Internet fanatic. Gamer, Anime lover</p>
         </div>
     )
 }
